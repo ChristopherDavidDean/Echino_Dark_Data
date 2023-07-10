55d88408e341f9172e9fb3a38557a95f7ea3b52f
Christopher Dean <christopherdaviddean@gmail.com>
2023-07-10 18:00:44 +0100

    Adjusted scripts to put functions in setup file

--- 0.Setup.R
+++ 0.Setup.R
@@ -1,6 +1,6 @@
 ################################################################################
 #                                                                              #
-# ECHINOID PAPERS DATA SETUP                                                   #
+# ECHINOID PAPERS DATA SETUP AND FUNCTIONS                                     #
 # Code by C. D. Dean, started 29.09.23                                         #
 #                                                                              #
 ################################################################################
@@ -30,6 +30,7 @@ library(deeptime)
 library(wesanderson)
 library(viridis)
 library(raster)
+library(MuMIn)
 
 # Set working directory
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
@@ -123,3 +124,94 @@ m.dat.rotate <- palaeorotate(m.dat.rotate,
              age = "bin_midpoint",
              model = "PALEOMAP",
              method = "point")
+
+################################################################################
+# 4. FUNCTIONS
+################################################################################
+
+# Bar plot
+make.bar.plot <- function(dataset, fill, legend, colour, flip = FALSE){
+  a <- ggplot(dataset) +
+    aes(x = Preservation_score, fill = fill) +
+    geom_bar() +
+    scale_fill_manual(values=wes_palette(colour)) +
+    guides(fill=guide_legend(title=legend)) +
+    ylab("Frequency") +
+    xlab("Preservation Score") +
+    theme_bw()
+  if(flip == TRUE){
+    a <- a + coord_flip()
+  }
+  return(a)
+}
+
+# Proportional bar plot
+make.prop.bar.plot <- function(dataset, fill, legend, colour, flip = FALSE){
+  a <- ggplot(dataset) +
+    aes(x = Preservation_score, fill = fill) +
+    geom_bar(position = 'fill') +
+    scale_fill_manual(values=wes_palette(colour)) +
+    guides(fill=guide_legend(title=legend)) +
+    ylab("Proportion of dataset") +
+    xlab("Preservation Score") +
+    theme_bw() +
+    scale_y_continuous(expand = c(0,0), limits = c(0,1))
+  if(flip == TRUE){
+    a <- a + coord_flip()
+  }
+  return(a)
+}
+
+# Set preservation score for Logistic Regression models
+set_Pres_score <- function(data, level){
+  # if score = 5, class as 1, otherwise class as 0.
+  data$LR_Pres_score <- 0
+  if(length(level) > 1){
+    for(n in 1:length(level)){
+      data$LR_Pres_score[data$Preservation_score == level[n]] <- 1
+    }
+  }else{
+    # Set values in 'new_column' to 1 where the original_column is equal to 5
+    data$LR_Pres_score[data$Preservation_score == level] <- 1
+  }
+  data$LR_Pres_score <- as.factor(data$LR_Pres_score)
+  data <- data
+}
+
+# Function to split out and reorganise data by preservation score for correlations
+split <- function(data, Var2, order = FALSE, stage = FALSE){
+  for(t in Var2){
+    temp.data <- filter(data, Var2 == t)
+    if(order == TRUE){
+      temp.data <- temp.data[match(order_ind, temp.data$Var1),]
+    }
+    if(stage == TRUE){
+      temp_stages <- stages[55:92,]
+      temp_stages$bin_midpoint <- (temp_stages$max_ma + temp_stages$min_ma)/2 
+      temp.data <- merge(temp_stages, temp.data, by = "bin_midpoint", all = TRUE)
+      temp.data$Preservation_score <- t
+      temp.data$Freq[is.na(temp.data$Freq)] <- 0
+    }
+    assign(paste(deparse(substitute(data)), t, sep = "."), temp.data, envir = .GlobalEnv)
+  }
+}
+
+# Function for making maps
+get_grid_im <- function(data, res, name, ext){ # Data is first output from combine_data (fossil.colls). Res is chosen resolution in degrees. name is user inputted string related to data inputted, for display on graphs. 
+  xy <- cbind(as.double(data$lng), as.double(data$lat))
+  #xy <- unique(xy)
+  r <- raster::raster(ext = ext, res = res)
+  r <- raster::rasterize(xy, r, fun = 'count')
+  #r[r > 0] <- 1 # Remove if you want values instead of pure presence/absence.
+  countries <- maps::map("world", plot=FALSE, fill = TRUE) # find map to use as backdrop
+  countries <<- maptools::map2SpatialPolygons(countries, IDs = countries$names, proj4string = CRS("+proj=longlat")) # Turn map into spatialpolygons
+  mapTheme <- rasterVis::rasterTheme(region=viridis(8))
+  print(rasterVis::levelplot(r, margin=F, par.settings=mapTheme,  main = paste("Total ", (substitute(name)), " per Grid Cell", sep = "")) + #create levelplot for raster
+          #   latticeExtra::layer(sp.polygons(states, col = "white", fill = NA), under = T)  + # Plots state lines
+          latticeExtra::layer(sp.polygons(countries, col = 0, fill = "light grey"), under = T)) # Plots background colour
+  hist(r, breaks = 20,
+       main = paste((substitute(name)), " per Grid Cell", sep = ""),
+       xlab = "Number of Collections", ylab = "Number of Grid Cells",
+       col = "springgreen")
+  r <<- r
+}
--- 1.Taphonomy_script.R
+++ 1.Taphonomy_script.R
@@ -18,43 +18,6 @@ source("0.Setup.R")
 ################################################################################
 
 #####################
-##### FUNCTIONS #####
-#####################
-
-# Bar plot
-make.bar.plot <- function(dataset, fill, legend, colour, flip = FALSE){
-  a <- ggplot(dataset) +
-    aes(x = Preservation_score, fill = fill) +
-    geom_bar() +
-    scale_fill_manual(values=wes_palette(colour)) +
-    guides(fill=guide_legend(title=legend)) +
-    ylab("Frequency") +
-    xlab("Preservation Score") +
-    theme_bw()
-  if(flip == TRUE){
-    a <- a + coord_flip()
-  }
-  return(a)
-}
-
-# Proportional bar plot
-make.prop.bar.plot <- function(dataset, fill, legend, colour, flip = FALSE){
-  a <- ggplot(dataset) +
-    aes(x = Preservation_score, fill = fill) +
-    geom_bar(position = 'fill') +
-    scale_fill_manual(values=wes_palette(colour)) +
-    guides(fill=guide_legend(title=legend)) +
-    ylab("Proportion of dataset") +
-    xlab("Preservation Score") +
-    theme_bw() +
-    scale_y_continuous(expand = c(0,0), limits = c(0,1))
-  if(flip == TRUE){
-    a <- a + coord_flip()
-  }
-  return(a)
-}
-
-#####################
 ##### LITHOLOGY #####
 #####################
 
@@ -1372,21 +1335,6 @@ plot(Effect(focal.predictors = c("p_lat", "Finalised_lith"),m[[1]]),
 ##### LOGISTIC REGRESSION #####
 ###############################
 
-set_Pres_score <- function(data, level){
-  # if score = 5, class as 1, otherwise class as 0.
-  data$LR_Pres_score <- 0
-  if(length(level) > 1){
-    for(n in 1:length(level)){
-      data$LR_Pres_score[data$Preservation_score == level[n]] <- 1
-    }
-  }else{
-    # Set values in 'new_column' to 1 where the original_column is equal to 5
-    data$LR_Pres_score[data$Preservation_score == level] <- 1
-  }
-  data$LR_Pres_score <- as.factor(data$LR_Pres_score)
-  data <- data
-}
-
 # Set preservation level to explore
 data.LR <- set_Pres_score(data, c(4,5))
 
@@ -1469,24 +1417,6 @@ div.period.gen <- binstat(genus.lvl,
                           coll = 'Locality')
 # Create order
 order_ind <- c("Permian", "Carboniferous", "Devonian", "Silurian", "Ordovician")
-
-# Function to split out and reorganise data by preservation score for correlations
-split <- function(data, Var2, order = FALSE, stage = FALSE){
-  for(t in Var2){
-    temp.data <- filter(data, Var2 == t)
-    if(order == TRUE){
-      temp.data <- temp.data[match(order_ind, temp.data$Var1),]
-    }
-    if(stage == TRUE){
-      temp_stages <- stages[55:92,]
-      temp_stages$bin_midpoint <- (temp_stages$max_ma + temp_stages$min_ma)/2 
-      temp.data <- merge(temp_stages, temp.data, by = "bin_midpoint", all = TRUE)
-      temp.data$Preservation_score <- t
-      temp.data$Freq[is.na(temp.data$Freq)] <- 0
-    }
-    assign(paste(deparse(substitute(data)), t, sep = "."), temp.data, envir = .GlobalEnv)
-  }
-}
 
 # Run function for each level
 split(period.pres.gen, period.pres.gen$Var2, order = TRUE)
@@ -1772,29 +1702,6 @@ write.csv(cor.results, "Macrostrat_PBDB_correlations.csv")
 ################################################################################
 # 8. MAPS
 ################################################################################
-
-####################
-##### FUNCTION #####
-####################
-
-get_grid_im <- function(data, res, name, ext){ # Data is first output from combine_data (fossil.colls). Res is chosen resolution in degrees. name is user inputted string related to data inputted, for display on graphs. 
-  xy <- cbind(as.double(data$lng), as.double(data$lat))
-  #xy <- unique(xy)
-  r <- raster::raster(ext = ext, res = res)
-  r <- raster::rasterize(xy, r, fun = 'count')
-  #r[r > 0] <- 1 # Remove if you want values instead of pure presence/absence.
-  countries <- maps::map("world", plot=FALSE, fill = TRUE) # find map to use as backdrop
-  countries <<- maptools::map2SpatialPolygons(countries, IDs = countries$names, proj4string = CRS("+proj=longlat")) # Turn map into spatialpolygons
-  mapTheme <- rasterVis::rasterTheme(region=viridis(8))
-  print(rasterVis::levelplot(r, margin=F, par.settings=mapTheme,  main = paste("Total ", (substitute(name)), " per Grid Cell", sep = "")) + #create levelplot for raster
-          #   latticeExtra::layer(sp.polygons(states, col = "white", fill = NA), under = T)  + # Plots state lines
-          latticeExtra::layer(sp.polygons(countries, col = 0, fill = "light grey"), under = T)) # Plots background colour
-  hist(r, breaks = 20,
-       main = paste((substitute(name)), " per Grid Cell", sep = ""),
-       xlab = "Number of Collections", ylab = "Number of Grid Cells",
-       col = "springgreen")
-  r <<- r
-}
 
 ##################
 ##### MODERN #####
